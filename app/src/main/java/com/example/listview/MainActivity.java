35daa776727fa00884773fbb3ca1f7132a6f94c6
karthiksb <34330740+karthiksb@users.noreply.github.com>
2022-03-27 12:29:22 +0530

    added setOnItemClickLister

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String selectedItem =(String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
